66273de7f4c1fae79e9da09b64e56daba9c40171
Jay <mrssfireball@gmail.com>
2020-03-22 17:53:44 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Spoicy !</title>
+    <meta name="description" content="Spoicy">
+
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <div class="container text-center main-container">
+        <div class="row justify-content-center">
+            <h1>Fireball</h1>
+        </div>
+        <div class="row justify-content-center">
+            <a href="https://www.twitch.tv/onlyfireball" target="_blank" type="button" class="btn btn-blue">Twitch</a>
+        </div>
+        <div class="row justify-content-center">
+            <a href="https://www.youtube.com/channel/UCsVw7FLt28Boqi7e6CnkoXg" target="_blank" type="button" class="btn btn-pink">YouTube</a>
+        </div>
+        <div class="row justify-content-center">
+            <a href="https://www.twitter.com/onlyfireball_" target="_blank" type="button" class="btn btn-white">Twitter</a>
+        </div>
+        <div class="row justify-content-center">
+            <a href="https://www.speedrun.com/user/fireball" target="_blank" type="button" class="btn btn-pink">Speedrun</a>
+        </div>
+        <div class="row justify-content-center">
+            <input type="button" class="btn btn-blue" value="Discord" onclick="window.alert('Discord: Fireball#4308');"/>
+        </div>
+    </div>
+    <script src="js/scripts.js"></script>
+</body>
+</html>
