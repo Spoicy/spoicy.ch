5a60c47eda65d7c2e00398fa216b62e58033adc2
spoicy <mrssfireball@gmail.com>
2020-07-11 13:48:05 +0200

    Add head data to linktree template

--- resources/views/linktree.blade.php
+++ resources/views/linktree.blade.php
@@ -4,6 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <title>Fireball</title>
+        <meta name="description" content="Spoicy">
+
         <link href="https://fonts.googleapis.com/css2?family=Karla:wght@400;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="css/app.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
