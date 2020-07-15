508faeb5798e44d14b68486f02fabddf375344e8
spoicy <mrssfireball@gmail.com>
2020-07-15 20:05:36 +0200

    Fix padding-bottom on container in linktree template

--- resources/views/linktree.blade.php
+++ resources/views/linktree.blade.php
@@ -12,7 +12,7 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     </head>
     <body class="pink">
-        <div class="container text-center main-container">
+        <div class="container text-center linktree-container">
             <div class="row">
                 <img src="img/fireball.png" alt="fireball.png" />
             </div>
