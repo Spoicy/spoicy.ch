903cc819ded5e69c6c7a60d8be18218c5ddf86c5
Spoicy <mrssfireball@gmail.com>
2020-07-21 17:31:33 +0200

    Add Typescript compatibility

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,19 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+mix.webpackConfig({
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                options: { appendTsSuffixTo: [/\.vue$/] },
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx', '.vue', '.ts', '.tsx'],
+    },
+});
