99d5b99173dbfc4d07ff0f9b3f8f2d2e0d0311ca
spoicy <mrssfireball@gmail.com>
2020-07-31 21:49:39 +0200

    Add blade functionality to SRDC template

--- resources/views/srdc.blade.php
+++ resources/views/srdc.blade.php
@@ -1,4 +1,4 @@
-<?php
+@php
 
 use App\Http\Controllers\SRDC;
 use Illuminate\Support\Facades\DB;
@@ -9,22 +9,19 @@ foreach($speedrunsQuery as $speedrun) {
     $speedruns[] = $speedrun;
 }
 $speedrunsFive = array_slice($speedruns, 0, 5);
-?>
+@endphp
 <h1>Speedruns</h1>
-<?php
-foreach ($speedrunsFive as $key => $speedrun) { ?>
+@foreach ($speedrunsFive as $key => $speedrun)
     <div class="row speedrun">
         <div class="col-lg-4 col-md-3 col-sm-4 col-5 speedrun-logo">
-            <img src="<?php echo $speedrun->image; ?>" alt="<?php echo $speedrun->game; ?>" />
+            <img src="{{$speedrun->image}}" alt="{{$speedrun->game}}" />
         </div>
         <div class="col-lg-8 col-md-9 col-sm-8 col-7 speedrun-text">
-            <a href="<?php echo $speedrun->game_link; ?>"><?php echo $speedrun->game; ?></a>
+            <a href="{{$speedrun->game_link}}">{{$speedrun->game}}</a>
             <br>
-            <a href="<?php echo $speedrun->category_link; ?>" class="speedrun-category"><?php echo $speedrun->category; ?></a>
-            <span class="speedrun-time"><?php echo SRDC::getTimeFormat($speedrun->time); ?></span>
-            <span class="speedrun-date"><?php echo SRDC::getDateFormat($speedrun->date); ?></span>
+            <a href="{{$speedrun->category_link}}" class="speedrun-category">{{$speedrun->category}}</a>
+            <span class="speedrun-time">{{SRDC::getTimeFormat($speedrun->time)}}</span>
+            <span class="speedrun-date">{{SRDC::getDateFormat($speedrun->date)}}</span>
         </div>
     </div>
-<?php
-}
-?>
+@endforeach
