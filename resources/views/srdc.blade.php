b8a1c65df574931c77527551348b39ff116e141f
Spoicy <mrssfireball@gmail.com>
2020-07-31 09:12:36 +0200

    Move SRDC into own template

---
+++ resources/views/srdc.blade.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\SRDC;
+use Illuminate\Support\Facades\DB;
+
+$speedrunsQuery = DB::table('speedruns')->get();
+$speedruns = array();
+foreach($speedrunsQuery as $speedrun) {
+    $speedruns[] = $speedrun;
+}
+$speedrunsFive = array_slice($speedruns, 0, 5);
+?>
+<h1>Speedruns</h1>
+<?php
+foreach ($speedrunsFive as $key => $speedrun) { ?>
+    <div class="row speedrun">
+        <div class="col-lg-4 col-md-12 col-sm-4 col-6 speedrun-logo">
+            <img src="<?php echo $speedrun->image; ?>" alt="<?php echo $speedrun->game; ?>" />
+        </div>
+        <div class="col-lg-8 col-md-12 col-sm-8 col-6 speedrun-text">
+            <a href="<?php echo $speedrun->game_link; ?>"><?php echo $speedrun->game; ?></a>
+            <br>
+            <a href="<?php echo $speedrun->category_link; ?>" class="speedrun-category"><?php echo $speedrun->category; ?></a>
+            <span class="speedrun-time"><?php echo SRDC::getTimeFormat($speedrun->time); ?></span>
+            <span class="speedrun-date"><?php echo SRDC::getDateFormat($speedrun->date); ?></span>
+        </div>
+    </div>
+<?php
+}
+?>
--- resources/views/stream.blade.php
+++ resources/views/stream.blade.php
@@ -18,15 +18,6 @@ use Illuminate\Support\Facades\DB;
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     </head>
     <body>
-    <?php 
-    $speedrunsQuery = DB::table('speedruns')->get();
-    $speedruns = array();
-    foreach($speedrunsQuery as $speedrun) {
-        $speedruns[] = $speedrun;
-    }
-    $speedrunsFive = array_slice($speedruns, 0, 5); 
-
-    ?>
         <div class="container stream-container">
             <div class="row">
                 <div class="col-md-6 col-xs-12"></div>
@@ -34,19 +25,8 @@ use Illuminate\Support\Facades\DB;
             </div>
             <div class="row">
                 <div class="col-md-6 col-xs-12"></div>
-                <div class="col-md-6 col-xs-12">
-                    <h1>Speedruns</h1>
-                    <?php 
-                    foreach ($speedrunsFive as $key => $speedrun) { ?>
-                        <img src="<?php echo $speedrun->image; ?>" alt="<?php echo $speedrun->game; ?>" />
-                        <a href="<?php echo $speedrun->game_link; ?>"><?php echo $speedrun->game; ?></a>
-                        <a href="<?php echo $speedrun->category_link; ?>" class="speedrun-category"><?php echo $speedrun->category; ?></a>
-                        <span class="speedrun-time"><?php echo SRDC::getTimeFormat($speedrun->time); ?></span>
-                        <span class="speedrun-date"><?php echo SRDC::getDateFormat($speedrun->date); ?></span>
-                        <br>
-                        <?php 
-                    }
-                    ?>
+                <div class="col-md-6 col-xs-12 speedrun-container">
+                    @include('srdc')
                 </div>
             </div>
         </div>
