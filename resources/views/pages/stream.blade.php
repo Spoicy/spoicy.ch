c25bb291f010a86f9178aeb6eb727cadc6fe5b70
spoicy <mrssfireball@gmail.com>
2022-05-25 12:19:20 +0200

    Add Twemoji support to social media stream page

--- resources/views/pages/stream.blade.php
+++ resources/views/pages/stream.blade.php
@@ -16,6 +16,7 @@ $i = 0;
         <link rel="stylesheet" href="css/app.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+        <script src="https://twemoji.maxcdn.com/v/latest/twemoji.min.js" crossorigin="anonymous"></script>
     </head>
     <body class="soft-blue">
         @include('components/other/overlay', ['nav' => 'hSocial'])
@@ -47,5 +48,6 @@ $i = 0;
             @endif
         </div>
         <script src="js/app.js"></script>
+        <script>twemoji.parse(document.body);</script>
     </body>
 </html>
