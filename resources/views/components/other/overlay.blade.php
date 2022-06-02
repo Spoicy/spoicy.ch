1f981c763a9d09357322bcb79f021f00d59f1ba2
spoicy <mrssfireball@gmail.com>
2022-06-02 20:22:46 +0200

    Make popout menu fixed and improve mobile responsivity

--- resources/views/components/other/overlay.blade.php
+++ resources/views/components/other/overlay.blade.php
@@ -8,23 +8,25 @@
     ];
 @endphp
 <div class="overlay-container">
-    <div class="background-dim" id="bDim"></div>
-    <div class="menuburger">
-        <button id="mbButton"><i class="fa fa-bars"></i></button>
-    </div>
-    <div class="github-ico">
-        <a class="fa fa-github" href="https://github.com/Spoicy/spoicy.ch" aria-label="Source code link" target="_blank" data-toggle="tooltip" data-placement="bottom" title="Source code"></a>
-    </div>
-    <div class="menu-popout" id="mPopout">
-        <button id="mcButton"><i class="fa fa-times"></i></button>
-        <ul>
-            @foreach ($links as $key => $data)
-                @if ($key == $nav)
-                    <li><a class="link-active" href="{{$data[0]}}" id="{{$key}}">{{$data[1]}}</a></li>
-                @else
-                    <li><a href="{{$data[0]}}" id="{{$key}}">{{$data[1]}}</a></li>
-                @endif
-            @endforeach
-        </ul>
+    <div class="inner-overlay">
+        <div class="background-dim" id="bDim"></div>
+        <div class="menuburger">
+            <button id="mbButton"><i class="fa fa-bars"></i></button>
+        </div>
+        <div class="github-ico">
+            <a class="fa fa-github" href="https://github.com/Spoicy/spoicy.ch" aria-label="Source code link" target="_blank" data-toggle="tooltip" data-placement="bottom" title="Source code"></a>
+        </div>
+        <nav class="menu-popout" id="mPopout">
+            <button id="mcButton"><i class="fa fa-times"></i></button>
+            <ul>
+                @foreach ($links as $key => $data)
+                    @if ($key == $nav)
+                        <li><a class="link-active" href="{{$data[0]}}" id="{{$key}}">{{$data[1]}}</a></li>
+                    @else
+                        <li><a href="{{$data[0]}}" id="{{$key}}">{{$data[1]}}</a></li>
+                    @endif
+                @endforeach
+            </ul>
+        </nav>
     </div>
 </div>
