8716c275b99e4237442653bfde92ab7db7670089
Jae <39260243+Spoicy@users.noreply.github.com>
2023-04-03 14:24:24 +0200

    Fix 404 page for non-post blog pages

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,6 +3,6 @@
 @section('title', __('Not Found'))
 @section('code', '404')
 @section('message', __('Not Found'))
-@if (str_contains(Request::url(), 'blog'))
+@if (str_contains(Request::url(), 'blog/post'))
     @section('additional', "No blog post could be found with this title.")
 @endif
